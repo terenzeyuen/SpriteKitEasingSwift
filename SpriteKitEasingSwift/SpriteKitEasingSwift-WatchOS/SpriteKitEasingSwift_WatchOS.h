819c41a006e494542037fc0f6e0820ff2ee5d085
Terenze Yuen <terenze.yuen@digitalworkroom.co.uk>
2018-05-04 17:35:28 +0100

    WatchOS

---
+++ SpriteKitEasingSwift/SpriteKitEasingSwift-WatchOS/SpriteKitEasingSwift_WatchOS.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKitEasingSwift_WatchOS.h
+//  SpriteKitEasingSwift-WatchOS
+//
+//  Created by Terenze Yuen on 04/05/2018.
+//  Copyright © 2018 Digital Workroom. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SpriteKitEasingSwift_WatchOS.
+FOUNDATION_EXPORT double SpriteKitEasingSwift_WatchOSVersionNumber;
+
+//! Project version string for SpriteKitEasingSwift_WatchOS.
+FOUNDATION_EXPORT const unsigned char SpriteKitEasingSwift_WatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKitEasingSwift_WatchOS/PublicHeader.h>
+
+
