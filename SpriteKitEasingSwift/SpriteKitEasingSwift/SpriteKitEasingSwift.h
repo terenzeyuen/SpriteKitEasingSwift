06a557c5102a8d6e6a8f3f441a85c8f495645e3a
Terenze Yuen <terenze.yuen@digitalworkroom.co.uk>
2018-05-04 17:33:02 +0100

    Carthage setup

---
+++ SpriteKitEasingSwift/SpriteKitEasingSwift/SpriteKitEasingSwift.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKitEasingSwift.h
+//  SpriteKitEasingSwift
+//
+//  Created by Terenze Yuen on 04/05/2018.
+//  Copyright © 2018 Digital Workroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpriteKitEasingSwift.
+FOUNDATION_EXPORT double SpriteKitEasingSwiftVersionNumber;
+
+//! Project version string for SpriteKitEasingSwift.
+FOUNDATION_EXPORT const unsigned char SpriteKitEasingSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKitEasingSwift/PublicHeader.h>
+
+
